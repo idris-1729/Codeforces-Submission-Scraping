442bf8ddcfbae21f2fd41c0367ce7962ac77d783
idris-1729 <sameer20@iitk.ac.in>
2024-01-08 18:58:36 +0530

    file_upload

---
+++ input.py
@@ -0,0 +1,4 @@
+yoursEmail = 'aakibalam0808@gmail.com'
+friendsList = ['PRO_fessor_', 'Mayank_Pushpjeet', 'udhavvarma03', 'satyam343', 'sultan__', 'onetap']
+my_emailId = 'enter your gmail here'
+password = 'enter your password here'
